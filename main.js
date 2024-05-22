bd8fa5d8ab7e0658e291507cca00b1d7564b12e8
Humaira Moin <159626452+humamoin70@users.noreply.github.com>
2024-05-21 22:15:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+// Define a types alias named product to represent a product with the following properties:
+// type product = {
+//         name: "string",
+//         price:number,
+//         inventory: {
+//                 stock:number,
+//                 colorOption:"string", 
+//         },
+//     };
+//         let product = [
+//             {
+//                name :"watches",
+//                price : 5000,
+//                inventory:{
+//                 stock : 5,
+//                 colorOption : ["black","white"],
+//                },
+//             },
+//         ];
+//         console.log(product)
+//         let arry[] = [
+//             {
+//                  name :"shoes ",
+//                  price : 2000,
+//                  inventory:{
+//                  stock : 3,
+//                  colorOption:["blue","red"]
+//             },
+//         },
+//     ];
+let shop = [
+    {
+        name: "Bags",
+        price: 3000,
+        inventory: {
+            stock: 20,
+            colorOption: ["Green", "light blue", "purple"]
+        },
+    },
+];
+console.log(shop);
